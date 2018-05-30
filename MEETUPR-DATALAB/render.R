a71d7d915f809cb9598b5fb34c42495e7576d56e
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-05-30 22:06:02 +0200

    last update

--- MEETUPR-DATALAB/render.R
+++ MEETUPR-DATALAB/render.R
@@ -1,7 +1,7 @@
 
 # render
 render_prez <- function(self_contained = FALSE) {
-  to_render <- here::here("MEETUPR_28-05-2018-DATALAB", "MEETUP_R-Rex_Datalab.Rmd")
+  to_render <- here::here("MEETUPR-DATALAB", "MEETUP_R-Rex_Datalab.Rmd")
   html_output <- if (self_contained) "self_contained.html" else "index.html"
   chakra <- if (self_contained) "libs/remark-latest.min.js" else "https://remarkjs.com/downloads/remark-latest.min.js"
   rmarkdown::render(
