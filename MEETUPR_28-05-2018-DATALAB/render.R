25ee30e790cd5dae9d276e7ea4d812c54b93073b
Christophe Dervieux <christophe.dervieux@gmail.com>
2018-05-30 21:30:55 +0200

    delete comments

--- MEETUPR_28-05-2018-DATALAB/render.R
+++ MEETUPR_28-05-2018-DATALAB/render.R
@@ -11,23 +11,5 @@ render_prez <- function(self_contained = FALSE) {
                           self_contained = self_contained),
     encoding = "UTF-8"
   )
-  # callr::r(function(to_render, html_output, chakra){
-  #   rmarkdown::render(
-  #     to_render,
-  #     output_file = html_output,
-  #     output_options = list(chakra = chakra,
-  #                           self_contained = self_contained),
-  #     encoding = "UTF-8"
-  #   )
-  # })
 }
 
-  # Self contained
-  # callr::r(function(){
-  #   rmarkdown::render(
-  #     "MEETUPR_28-05-2018-DATALAB/MEETUP_R-Rex_Datalab.Rmd",
-  #     output_file = "self_contained.html",
-  #     output_options = list(chakra = "libs/remark-latest.min.js", self_contained = TRUE),
-  #     encoding = "UTF-8"
-  #   )
-  # })
