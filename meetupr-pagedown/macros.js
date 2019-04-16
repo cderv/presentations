26d9eaaf6ea9e2b109aaaa6499520641e292cd6f
Christophe Dervieux <christophe.dervieux@gmail.com>
2019-04-17 00:22:06 +0200

    Ajout d'une macro scaleh

--- meetupr-pagedown/macros.js
+++ meetupr-pagedown/macros.js
@@ -2,3 +2,7 @@ remark.macros.scale = function(w) {
   var url = this;
   return '<img src="' + url + '" style="width: ' + w + '" />';
 };
+remark.macros.scaleh = function(h) {
+  var url = this;
+  return '<img src="' + url + '" style="height: ' + h + '" />';
+};
